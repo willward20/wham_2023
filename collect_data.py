b5975a1ef556753bf55bfd0aa239072963a680d5
Lin Zhang <linzhank@gmail.com>
2022-04-08 08:42:18 -0500

    create train

--- collect_data.py
+++ collect_data.py
@@ -7,7 +7,7 @@ from datetime import datetime
 now = datetime.now()  # current date and time
 d = now.strftime("%m%d%H%M")
 save_path = "data_" + d
-classes = ["forward", "fleft", "fright", "stay"]
+classes = ["forward", "fleft", "fright"]
 if not os.path.exists(save_path):
     os.makedirs(save_path)
     for c in classes:
@@ -30,11 +30,7 @@ while True:
     im = cv.resize(frame, (150, 150))
     # take down key pressed
     k = cv.waitKey(1) & 0xFF
-    if k == ord("i"):
-        th = 1
-        cv.imwrite(f"{save_path}/forward/{i}.jpg", im)
-        print("forward")
-    elif k == ord("u"):
+    if k == ord("u"):
         th = 1
         st = 1
         cv.imwrite(f"{save_path}/fleft/{i}.jpg", im)
@@ -49,7 +45,10 @@ while True:
         st = 0
         break
     else:
-        cv.imwrite(f"{save_path}/stay/{i}.jpg", im)
+        th = 1
+        st = 0
+        cv.imwrite(f"{save_path}/forward/{i}.jpg", im)
+        print("forward")
 
     act_buf.append([th, st])
     # operations on the frame
@@ -58,6 +57,5 @@ while True:
 # When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
-d = now.strftime("%m%d%H%M")
-with open(f"{d}.npy", "wb") as f:
-    np.save(f, np.array(act_buf))
+# with open(f"{d}.npy", "wb") as f:
+#     np.save(f, np.array(act_buf))
