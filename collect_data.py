c4ec64e4be0bd3048bfa4ae9ecdc168e513397e2
Lin Zhang <linzhank@gmail.com>
2022-04-08 00:58:12 -0500

    update data collect, create classes folder

--- collect_data.py
+++ collect_data.py
@@ -7,8 +7,11 @@ from datetime import datetime
 now = datetime.now()  # current date and time
 d = now.strftime("%m%d%H%M")
 save_path = "data_" + d
+classes = ["forward", "fleft", "fright", "stay"]
 if not os.path.exists(save_path):
     os.makedirs(save_path)
+    for c in classes:
+        os.makedirs(f'{save_path}/{c}')
 
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
@@ -24,25 +27,32 @@ while True:
     if not ret:
         print("Can't receive frame")
         break
+    im = cv.resize(frame, (150, 150))
     # take down key pressed
     k = cv.waitKey(1) & 0xFF
     if k == ord("i"):
         th = 1
+        cv.imwrite(f"{save_path}/forward/{i}.jpg", im)
         print("forward")
     elif k == ord("u"):
         th = 1
         st = 1
+        cv.imwrite(f"{save_path}/fleft/{i}.jpg", im)
         print("forward left")
     elif k == ord("o"):
         th = 1
         st = -1
+        cv.imwrite(f"{save_path}/fright/{i}.jpg", im)
         print("forward right")
     elif k == ord("q"):
+        th = 0
+        st = 0
         break
+    else:
+        cv.imwrite(f"{save_path}/stay/{i}.jpg", im)
+
     act_buf.append([th, st])
     # operations on the frame
-    im = cv.resize(frame, (150, 150))
-    cv.imwrite(f"{save_path}/{i}.jpg", im)
     cv.imshow("image", im)
     i += 1
 # When everything done, release the capture
