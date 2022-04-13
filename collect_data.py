298d65477d3606518100e2f7eee56065e68e52fb
linzhanguca <lzhang12@uca.edu>
2022-04-13 09:42:10 -0500

    update data collection
    set state for actions so that next frame will keep previous action if no
    key pressed.

--- collect_data.py
+++ collect_data.py
@@ -2,8 +2,10 @@ import os
 import numpy as np
 import cv2 as cv
 from datetime import datetime
+from gpiozero import LED, Robot
 
 
+# create data storage
 now = datetime.now()  # current date and time
 d = now.strftime("%m%d%H%M")
 save_path = "data_" + d
@@ -12,50 +14,60 @@ if not os.path.exists(save_path):
     os.makedirs(save_path)
     for c in classes:
         os.makedirs(f'{save_path}/{c}')
-
+# init robot, Waveshare Motor Driver Board
+en_l = LED(26)
+en_r = LED(12)
+bot = Robot((20, 21), (6, 13))
+# init camera, webcam
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
-
-act_buf = []
-th = 0  # throttle
-st = 0  # steering
+cap.set(cv.CAP_PROP_FPS, 20)
 i = 0
+act = 0  # stop
+
+# Main
 while True:
     ret, frame = cap.read()
     if not ret:
         print("Can't receive frame")
         break
-    im = cv.resize(frame, (150, 150))
-    # take down key pressed
+    im = cv.resize(frame, (200, 200))
+    # set action according to key pressed
     k = cv.waitKey(1) & 0xFF
-    if k == ord("u"):
-        th = 1
-        st = 1
-        cv.imwrite(f"{save_path}/fleft/{i}.jpg", im)
-        print("forward left")
-    elif k == ord("o"):
-        th = 1
-        st = -1
-        cv.imwrite(f"{save_path}/fright/{i}.jpg", im)
-        print("forward right")
-    elif k == ord("q"):
-        th = 0
-        st = 0
+    if k == ord("q"):
+        bot.stop()
+        print("Data collection terminated!")
         break
-    else:
-        th = 1
-        st = 0
+    elif k == ord("u"):
+        act = 2  # forward left
+    elif k == ord("o"):
+        act = 3  # forward right
+    elif k == ord("i"):
+        act = 1  # forward
+    # send driving command and record corresponding image
+    if act == 0:
+        bot.stop()
+        continue
+    elif act == 1:
+        bot.forward(0.64)
         cv.imwrite(f"{save_path}/forward/{i}.jpg", im)
-        print("forward")
-
-    act_buf.append([th, st])
-    # operations on the frame
-    cv.imshow("image", im)
+    elif act == 2:
+        bot.left_motor.forward(0.32)
+        bot.right_motor.forward(0.64)
+        cv.imwrite(f"{save_path}/fleft/{i}.jpg", im)
+    elif act == 3:
+        bot.left_motor.forward(0.64)
+        bot.right_motor.forward(0.32)
+        cv.imwrite(f"{save_path}/fright/{i}.jpg", im)
+    # show image, TODO: headless
     i += 1
-# When everything done, release the capture
+    cv.imshow('view', im)
+
+# When everything done, release the capture, disable the robot
 cap.release()
 cv.destroyAllWindows()
-# with open(f"{d}.npy", "wb") as f:
-#     np.save(f, np.array(act_buf))
+bot.stop()
+en_l.off()
+en_r.off()
