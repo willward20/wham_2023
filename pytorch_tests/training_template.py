638173ab06e3a69e6acaedf12a00b94737208553
Will Ward <wward@cub.uca.edu>
2022-10-25 17:02:04 -0500

    finish training

--- pytorch_tests/training_template.py
+++ pytorch_tests/training_template.py
@@ -30,44 +30,119 @@ class CustomImageDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
-        steering = self.img_labels.iloc[idx, 1]
-        throttle = self.img_labels.iloc[idx, 2]
+        image = read_image(img_path) / 255 
+        steering = self.img_labels.iloc[idx, 1].astype(np.float32)
+        throttle = self.img_labels.iloc[idx, 2].astype(np.float32)
         if self.transform:
             image = self.transform(image)
         #if self.target_transform:
         #    label = self.target_transform(label)
-        return image, steering, throttle
+        return image.float(), steering, throttle
 
 
 # Create a dataset
-annotations_file = "data.csv"  # the name of the csv file
-img_dir = "images"  # the name of the folder with all the images in it
+annotations_file = "data2022-10-18-16-00/labels.csv"  # the name of the csv file
+img_dir = "data2022-10-18-16-00/images"  # the name of the folder with all the images in it
 donkey_data = CustomImageDataset(annotations_file, img_dir)
 
 # Load the dataset
-batch_size = 64
+batch_size = 200
 train_dataloader = DataLoader(donkey_data, batch_size=batch_size, shuffle=False)
-train_features, steering, throttle = next(iter(train_dataloader))
-#train_features, steering, throttle = next(iter(train_dataloader))
-#train_features, steering, throttle = next(iter(train_dataloader))
-#train_features, steering, throttle = next(iter(train_dataloader))
 
-# Set X and y
-X = train_features
-print(X.size())
-y = torch.stack((steering, throttle), -1) # combines steering and throttle outputs (2 columns, X rows)
-y = y.float()
+"""
+# Test
+i = 1
+for X, steering, throttle in train_dataloader:
+    print(f"Shape of X in batch {i} [N, C, H, W]: {X.shape}")
+    print(f"Shape of steering in batch {i}: {steering.shape}")
+    print(f"Shape of throttle in batch {i}: {throttle.shape}")
+    i += 1
+    # break
+"""
 
-# Flatten and standardize
-M_train = X.shape[0]
-X_flatten = X.reshape(M_train, -1) # torch.Size([M_train, 3*120*160])
-X = X_flatten / 255.0
+# Define Neural Network
+class NeuralNetwork(nn.Module):
 
-# Split data into test and train
-test_size = 0.2 # percentage of data for testing
-X_train, X_test, y_train, y_test  = train_test_split(X, y, test_size=test_size, random_state=1234)
+    def __init__(self, hidden_layer_sizes):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(60*80, hidden_layer_sizes[0]),
+            nn.ReLU(),
+            nn.Linear(hidden_layer_sizes[0], hidden_layer_sizes[1]),
+            nn.ReLU(),
+            nn.Linear(hidden_layer_sizes[1], 2)
+        )
+    def forward(self, x):
+        x = self.flatten(x)
+        y_predicted = self.linear_relu_stack(x) 
+        return y_predicted # AKA logits
+    
 
+# Define Training Function
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    
+    for batch, (X, steering, throttle) in enumerate(dataloader):
+        #Combine steering and throttle into one tensor (2 columns, X rows)
+        y = torch.stack((steering, throttle), -1) 
+        #y = y.float()
+        
+        # Compute prediction error
+        pred = model(X)  # forward propagation
+        loss = loss_fn(pred, y)  # compute loss
+        optimizer.zero_grad()  # zero previous gradient
+        loss.backward()  # back propagatin
+        optimizer.step()  # update parameters
+        
+        if batch % 10 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+        
+
+# Define a test function to evaluate model performance
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, accuracy = 0, 0
+    with torch.no_grad():
+        for X, steering, throttle in dataloader:
+
+            #Combine steering and throttle into one tensor (2 columns, X rows)
+            y = torch.stack((steering, throttle), -1) 
+            y = y.float()
+            
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            accuracy += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    accuracy /= size
+    print(f"Test Error: \n Accuracy: {(100*accuracy):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+    return accuracy
+
+# Start training
+learning_rate = 0.001
+
+model = NeuralNetwork([256, 256])
+loss_fn = nn.MSELoss() # binary cross entropy loss
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate) 
+
+epochs = 5
+test_acc = []
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    #acc = test(test_dataloader, model, loss_fn)
+    #test_acc.append(acc)
+print("Done!")
+
+#plt.plot(test_acc)
+
+"""
 # Scale our features
 sc = StandardScaler() # makes features to have zero mean and unit variance
 X_train = sc.fit_transform(X_train)
@@ -76,60 +151,4 @@ X_test = sc.transform(X_test)
 # Convert test/train data back to tensors
 X_train = torch.from_numpy(X_train.astype(np.float32))
 X_test = torch.from_numpy(X_test.astype(np.float32))
-
-# Set constants
-n_features = X_train.shape[1]
-print("n_features: ", n_features)
-output_size = y_train.shape[1]
-print("output_size: ", output_size)
-
 """
-Step 2: Building the Neural Network Model
-"""
-
-# Step 1: Set up the model
-# model is linear combination of weights and bias
-# then apply sigmoid function at the end
-class LogisticRegression(nn.Module):
-    def __init__(self, n_input_features, n_output_features):
-        super(LogisticRegression, self).__init__()
-        self.linear = nn.Linear(n_input_features, n_output_features) # 2 values at the end of the model
-
-    def forward(self, x):
-        y_predicted = torch.sigmoid(self.linear(x))
-        return y_predicted
-
-model = LogisticRegression(n_features, output_size)
-
-# Step 2: Loss and Optimizer
-learning_rate = 0.01
-criterion = nn.BCELoss() # binary cross entropy loss
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-
-# Step 3: Training Loop
-num_epochs = 100
-for epoch in range(num_epochs):
-    # forward pass and loss calcualtion
-    y_predicted = model(X_train)
-    loss = criterion(y_predicted, y_train)
-
-    # backward pass
-    loss.backward() # calculates gradients
-
-    # updates
-    optimizer.step() # pytorch does all udpate calcualtions for us
-
-    # zero gradients
-    optimizer.zero_grad()
-
-    if (epoch+1) % 10 == 0:
-        print(f'epoch: {epoch+1}, loss = {loss.item():.4f}')
-
-# evaluate the model
-with torch.no_grad():
-    y_predicted = model(X_test)
-
-    # sigmoid returns value between 0 and 1, so convert
-    y_predicted_cls = y_predicted.round()
-    acc = y_predicted_cls.eq(y_test).sum() / float(y_test.shape[0]) # sum all the correct predictions 
-    print(f'accuracy = {acc:.4f}')
