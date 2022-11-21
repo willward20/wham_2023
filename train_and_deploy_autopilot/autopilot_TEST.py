a65a15fde4ae4c28576d0bc41a5ac25e7ad7f2a0
wham pi <wham@pi.com>
2022-11-20 18:06:55 -0600

    update

--- train_and_deploy_autopilot/autopilot_TEST.py
+++ train_and_deploy_autopilot/autopilot_TEST.py
@@ -5,16 +5,24 @@ import motor
 from torchvision.io import read_image
 from torchvision.transforms import ToTensor, Resize
 import torch
-from cnn_network import cnn_network
+#from cnn_network import cnn_network
+from neural_network_class import NeuralNetwork
 from adafruit_servokit import ServoKit
+import json
 
 #pca.frequency = 50
 kit = ServoKit(channels=16)
 
+# Load in configuration constants for throttle and steering
+f = open('config.json')
+data = json.load(f)
+steering_trim = data['steering_trim']
+throttle_lim = data['throttle_trim']
+
 
 # Load model
-model = cnn_network()
-model.load_state_dict(torch.load("model_cnn.pth", map_location=torch.device('cpu')))
+model = NeuralNetwork([250, 250])
+model.load_state_dict(torch.load("STILL_MODEL.pth", map_location=torch.device('cpu')))
 
 # Setup Transforms
 img2tensor = ToTensor()
@@ -29,29 +37,29 @@ while True:
     ret, frame = cap.read()   
     if frame is not None:
         # cv.imshow('frame', frame)  # debug
-        print(f"original frame size: {frame.shape}")
+        #print(f"original frame size: {frame.shape}")
         #print("original frame: ", frame)
         frame = cv.resize(frame, (int(frame.shape[1]), int(frame.shape[0]))) # THIS LINE DOES NOTHING
-        print(f"resized frame size: {frame.shape}")
+        #print(f"resized frame size: {frame.shape}")
         #print("resized frame: ", frame)
         gray = frame  # we changed this from b&w to color
         img_tensor = img2tensor(gray) # this also divides by 255
-        print(f"frame to tensor size: {img_tensor.size()}")
+        #print(f"frame to tensor size: {img_tensor.size()}")
         #print("frame to tensor: ", img_tensor)
         img_tensor = resize(img_tensor) # THIS LINE IS NOT NEEDED (we don't need to resize the tensor. It already matches what's given in train.py
-        print(f"frame to tensor after resize: {img_tensor.size()}")
+        #print(f"frame to tensor after resize: {img_tensor.size()}")
         #print(img_tensor.shape)
     with torch.no_grad():
         img_tensor = img_tensor.unsqueeze(dim=0) # THIS LINE ADDS THE EXTRA DIMENSION
-        print(f"frame unsqueezed size: {img_tensor.size()}")
+        #print(f"frame unsqueezed size: {img_tensor.size()}")
         pred = model(img_tensor)
     print(pred)
-    steering, throttle = pred[0][0].item() + 0.71, pred[0][1].item()
+    steering, throttle = pred[0][0].item(), pred[0][1].item() # fixed so steering and throttle are assigned correrctly
     print("steering: ", steering)
     print("throttle: ", throttle)
-    motor.drive(throttle * 650 * 2.5) # we remove the negative before throttle so it doesn't drive bkwards
-    print("motor: ", throttle * 650 * 2.5)
-    ang = 90 * (1 + steering) + 6.8
+    motor.drive(throttle * throttle_lim) # we remove the negative before throttle so it doesn't drive bkwards
+    print("motor: ", throttle * throttle_lim)
+    ang = 90 * (1 + steering) + steering_trim
     if ang > 180:
         ang = 180
     elif ang < 0:
